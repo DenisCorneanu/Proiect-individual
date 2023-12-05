cd6ca69f612ba202a4fe7898d8e1b0cc33f4d13b
DenisEmanuel <corneanudenis1@gmail.com>
2023-12-05 11:27:12 +0200

    PI Demo Proiect

---
+++ Online Shopping Portal project/shopping/includes/myaccount-sidebar.php
@@ -0,0 +1,19 @@
+	<div class="col-md-4">
+					<!-- checkout-progress-sidebar -->
+<div class="checkout-progress-sidebar ">
+	<div class="panel-group">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+		    	
+		    </div>
+		    <div class="panel-body">
+				<ul class="nav nav-checkout-progress list-unstyled">
+					<li><a href="my-account.php">Contul meu</a></li>
+					<li><a href="bill-ship-addresses.php">Adresa de livrare / facturare</a></li>
+					<li><a href="order-history.php">Istoric comenzi</a></li>
+				</ul>		
+			</div>
+		</div>
+	</div>
+</div> 
+<!-- checkout-progress-sidebar -->				</div>
